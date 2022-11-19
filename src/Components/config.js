8b92c32f990e83fb6cd681ac7461f5c8884b9d06
IswaryaAnandhan <aishwariyaanandhan@gmail.com>
2022-11-19 20:57:33 +0530

    add changes

--- src/Components/config.js
+++ src/Components/config.js
@@ -1,6 +1,6 @@
 
 
 export const config={
-api:"https://nodeproductdetails.herokuapp.com"
-//  api:"http://localhost:3008"
+// api:"https://nodeproductdetails.herokuapp.com"
+ api:"http://localhost:3008"
 }
